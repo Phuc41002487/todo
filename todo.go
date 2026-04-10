2d0488b7cfda9099562ef259cd48c679a54328ed
Phuc Vo <phuc41002487@gmail.com>
2026-04-11 00:39:26 +0200

    feat: add help subcommand

--- todo.go
+++ todo.go
@@ -153,17 +153,41 @@ func List() {
     fmt.Println(string(data))
 }
 
+// Help function show the information about the app and how to use it
+func Help() {
+    fmt.Println(`todo is a cli app used to manage task that you want to do.
+Currently the todo app support below command
+  - help: show information about the app and supported subcommand
+  - list: show the current tasks and and their status.
+  - add: add task to todo list. task will be added with undone status.
+      If your task name include comprised of multiple words. Please enclosed them in double quote.
+      You can add multiple task at a time. Please separate the task by using space.
+  - change: change the status of the task.
+          Please enter your task name first and then the status you want to change it to
+          Task status can only Done or Undone.
+  - delete: delete a task by task name.`)
+}
+
 func main() {
-    switch os.Args[1] {
-    case "clear":
-        os.Create(FilePath)
-    case "add":
-        Add(os.Args[2:])
-    case "list":
-        List()
-    case "change":
-        Change(os.Args[2:])
-    case "delete":
-        Delete(os.Args[2:])
+    if len(os.Args) == 1 {
+        Help()
+    } else {
+        switch os.Args[1] {
+        case "clear":
+            os.Create(FilePath)
+        case "add":
+            Add(os.Args[2:])
+        case "list":
+            List()
+        case "change":
+            Change(os.Args[2:])
+        case "delete":
+            Delete(os.Args[2:])
+        case "help":
+            Help()
+        default:
+            fmt.Println("Invalid Command was enter. " + 
+            "Please use help subcommand to know how to use the app")
+        }
     }
 }
